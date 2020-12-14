9f1fc9b12aefdfbc3d635f82769bc36c9ec04444
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-14 16:20:05 +0530

    Made Changes

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ const onePageArticleCount = 10;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const isnullorUndefined = (val) => {
-  return;
-  val === null || val === undefined;
+  return val === null || val === undefined;
 };
 app.get("/newFeeds", async (req, res) => {
   const limit = req.body.limit;
   const offset = req.body.offset;
-
+  console.log(isnullorUndefined(limit), isnullorUndefined(this.offset));
   if (isnullorUndefined(limit) && isnullorUndefined(offset)) {
     const data = await newsArticleModel.find().skip().limit(10);
     res.send(data);
@@ -35,17 +34,17 @@ app.get("/newFeeds", async (req, res) => {
       res.send(data);
     }
   }
-  // if (!isnullorUndefined(limit)) {
-  //   const data = await newsArticleModel.find().skip(offset).limit(limit);
-  //   res.send(data);
-  // }
-  // if (!isnullorUndefined(offset)) {
-  //   const data = await newsArticleModel
-  //     .find()
-  //     .skip(offset)
-  //     .limit(onePageArticleCount);
-  //   res.send(data);
-  // }
+  if (!isnullorUndefined(limit)) {
+    const data = await newsArticleModel.find().skip(offset).limit(limit);
+    res.send(data);
+  }
+  if (!isnullorUndefined(offset)) {
+    const data = await newsArticleModel
+      .find()
+      .skip(offset)
+      .limit(onePageArticleCount);
+    res.send(data);
+  }
 });
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
