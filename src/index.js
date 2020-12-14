5d8a3edad77d898f909700f1e6a98c2542874d59
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-14 16:03:48 +0530

    remove data from import statement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const data = require("./data");
+
 const { newsArticleModel } = require("./connector");
 
 const onePageArticleCount = 10;
