8916ac7b5cd940b289f3f286ab60e0c73baf5b63
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-15 00:59:07 +0530

    used Ternary Operator

--- src/index.js
+++ src/index.js
@@ -1,41 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-
 const { newsArticleModel } = require("./connector");
-
 const onePageArticleCount = 10;
 
 // Parse JSON bodies (as sent by API clients)
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/newFeeds", async (req, res) => {
-  const { limit, offset } = req.body;
-  const limits = limit === "undefined" ? 10 : isNaN(limit) ? 10 : limit;
-  const offsets = offset === "undefined" ? 0 : isNaN(limit) ? 0 : offset;
+  // const { limit, offset } = req.query;
+  const limit = req.query.limit;
+  const offset = req.query.offset;
+  console.log(limit, offset);
+  const limits = limit == "undefined" ? 10 : isNaN(limit) ? 10 : Number(limit);
+  const offsets =
+    offset == "undefined" ? 0 : isNaN(offset) ? 0 : Number(offset);
+
   const value = await newsArticleModel.find().skip(offsets).limit(limits);
   res.send(value);
-  // console.log(isnullorUndefined(limit), isnullorUndefined(this.offset));
-  // const limit = limits==="undefined"?10:isNaN(limits)?
-  // if (isnullorUndefined(limit) && isnullorUndefined(offset)) {
-  //   const data = await newsArticleModel.find().skip().limit(10);
-  //   res.send(data);
-  // }
-  // if (!isnullorUndefined(limit) || !isnullorUndefined(offset)) {
-  //   if (!isnullorUndefined(limit) && !isnullorUndefined(offset)) {
-  //     const data = await newsArticleModel.find().skip(offset).limit(limit);
-  //     res.send(data);
-  //   }
-  //   if (!isnullorUndefined(limit)) {
-  //     const data = await newsArticleModel.find().skip().limit(limit);
-  //     res.send(data);
-  //   }
-  //   if (!isnullorUndefined(offset)) {
-  //     const data = await newsArticleModel.find().skip(offset).limit(10);
-  //     res.send(data);
-  //   }
-  // }
 });
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
