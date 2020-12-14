d1b210b206229fe6713f802d57a23f9a13c8b695
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-14 13:05:43 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
+const data = require("./data");
+const { newsArticleModel } = require("./connector");
 
-const onePageArticleCount = 10
-
+const onePageArticleCount = 10;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const isnullorUndefined = (val) => {
+  return val == null || val == undefined;
+};
+app.get("/newFeeds", async (req, res) => {
+  try {
+    const limit = req.body.limit;
+    const offset = req.body.offset;
 
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+    if (isnullorUndefined(limit) && isnullorUndefined(offset)) {
+      const data = await newsArticleModel.find().skip().limit(10);
+      res.send(data);
+    }
+    if (!isnullorUndefined(limit) && !isnullorUndefined(offset)) {
+      const data = await newsArticleModel.find().skip(offset).limit(limit);
+    }
+    if (!isnullorUndefined(limit)) {
+      const data = await newsArticleModel.find().skip(offset).limit(limit);
+      res.send(data);
+    }
+    if (!isnullorUndefined(offset)) {
+      const data = await newsArticleModel
+        .find()
+        .skip(offset)
+        .limit(onePageArticleCount);
+      res.send(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
